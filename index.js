c4becfe67105d86ec6948f8fa7cde3be3d97e4b5
Azani David Pinkney <info@azani.org>
2023-10-15 16:40:59 -0700

    object properties lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "John",
+    streetAddress: "1201 North Ogden Drive",
+}
+  
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    const employeeUpdate = {...employee};
+    employeeUpdate[name] = streetAddress;
+    return employeeUpdate;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    employee[name] = streetAddress;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, name) {
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name;
+    return employee;
+}
